049f22f231ade3c404ab2ddd1c51f344ffdfa676
Nikita Shchanin <shchanin.n@google.com>
2025-05-14 20:50:53 +0300

    Добавлены расчёты индикторов облигации

--- internal/securities/securities.go
+++ internal/securities/securities.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"math"
 	"net/http"
 	"simple-invest/internal/database"
 	"simple-invest/internal/servicelog"
@@ -17,6 +18,7 @@ const (
 	defaultFrom = "1997-01-01"
 	defaultTill = "2100-12-31"
 	taxRate     = 0.13
+	precision   = 4
 )
 
 var (
@@ -24,19 +26,20 @@ var (
 )
 
 type bondIndicators struct {
-	Isin            string    `json:"isin"`              // Ценная бумага
-	Facevalue       float64   `json:"facevalue"`         // Текущая номинальная стоимость
-	Aci             float64   `json:"aci"`               // НКД
-	Value           float64   `json:"value"`             // Сумма купона
-	PercentPrice    float64   `json:"percent_price"`     // Цена в процентах
-	Price           float64   `json:"price"`             // Цена
-	DaysToEvent     int64     `json:"days_to_event"`     // Дней до события
-	RedemtionDate   time.Time `json:"redemtion_date"`    // Дата погашения
-	SimpleYield     float64   `json:"simple_yield"`      // Простая доходоность
-	NetSimpleYield  float64   `json:"net_simple_yield"`  // Итоговая простая доходность
-	CurrentYield    float64   `json:"current_yield"`     // Текущая доходность
-	NetCurrentYield float64   `json:"net_current_yield"` // Итоговая текущая доходность
-	TaxRedemption   float64   `json:"tax_redemption"`    // Налог при погашении
+	Isin            string  `json:"isin"`              // Ценная бумага
+	FaceValue       float64 `json:"facevalue"`         // Текущая номинальная стоимость
+	AccruedInt      float64 `json:"accruedint"`        // НКД
+	Coupon          float64 `json:"coupon"`            // Сумма купона
+	PercentPrice    float64 `json:"percent_price"`     // Цена в процентах
+	Price           float64 `json:"price"`             // Цена
+	DaysToEvent     int64   `json:"days_to_event"`     // Дней до события
+	MatDate         string  `json:"matdate"`           // Дата погашения
+	OfferDate       string  `json:"offerdate"`         // Дата оферты
+	SimpleYield     float64 `json:"simple_yield"`      // Простая доходоность
+	NetSimpleYield  float64 `json:"net_simple_yield"`  // Итоговая простая доходность
+	CurrentYield    float64 `json:"current_yield"`     // Текущая доходность
+	NetCurrentYield float64 `json:"net_current_yield"` // Итоговая текущая доходность
+	MaturityTax     float64 `json:"maturity_tax"`      // Налог при погашении
 }
 
 type BondPayments struct {
@@ -48,6 +51,10 @@ type BondPayments struct {
 		Columns []string        `json:"columns"` // Названия колонок
 		Data    [][]interface{} `json:"data"`
 	} `json:"amortizations"`
+}
+
+func init() {
+	cl = gomoex.NewISSClient(http.DefaultClient)
 }
 
 type Coupon struct {
@@ -73,25 +80,26 @@ type Amortization struct {
 }
 
 type Bond struct {
-	Isin          string  `json:"isin"`          // ISIN код
-	ShortName     string  `json:"SHORTNAME"`     // Краткое наименование
-	Accruedint    string  `json:"ACCRUEDINT"`    // НКД на дату расчетов, в валюте расчетов
-	FaceValue     float64 `json:"FACEVALUE"`     // Номинальная (остаточная) стоимость
-	MatDate       string  `json:"MATDATE"`       // Дата погашения
-	CouponPeriod  int16   `json:"COUPONPERIOD"`  // Купонный период
-	SecName       string  `json:"SECNAME"`       // Полное наимнование
-	FaceUnit      string  `json:"FACEUNIT"`      // Уточнить (возоможно, валюта)
-	CouponPercent float64 `json:"COUPONPERCENT"` // Ставка купона (уточнить по обл. с переменным купоном и флоатерам)
-	OfferDate     string  `json:"OFFERDATE"`     // Дата оферты
-	SettleDate    string  `json:"SETTLEDATE"`    // Дата расчётов сделки
+	Isin          string  `json:"isin"`                // ISIN код
+	ShortName     string  `json:"shortname"`           // Краткое наименование
+	AccruedInt    float64 `json:"accruedint"`          // НКД на дату расчетов, в валюте расчетов
+	FaceValue     float64 `json:"facevalue"`           // Номинальная (остаточная) стоимость
+	MatDate       string  `json:"matdate"`             // Дата погашения
+	CouponPeriod  int32   `json:"couponperiod"`        // Купонный период
+	CouponPercent float64 `json:"couponpercent"`       // Ставка купона (уточнить по обл. с переменным купоном и флоатерам)
+	CouponValue   float64 `json:"couponvalue"`         // Сумма купона
+	SecName       string  `json:"secname"`             // Полное наимнование
+	FaceUnit      string  `json:"faceunit"`            // Уточнить (возоможно, валюта)
+	OfferDate     string  `json:"offerdate,omitempty"` // Дата оферты
+	SettleDate    string  `json:"settledate"`          // Дата расчётов сделки
 }
 
-func init() {
-	cl = gomoex.NewISSClient(http.DefaultClient)
+type BondMarketData struct {
+	Last float64 `json:"last"` //цена
+	// Value float64 //цена в валюте номинала
 }
 
 func boardSecuritiesMOEX(engine, market string) ([]gomoex.Security, error) {
-
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 
@@ -110,7 +118,6 @@ func boardSecuritiesMOEX(engine, market string) ([]gomoex.Security, error) {
 	}
 
 	return table, nil
-
 }
 
 func DownloadShares() (err error) {
@@ -240,9 +247,11 @@ func Dividends(ctx context.Context, isin string) ([]gomoex.Dividend, error) {
 }
 
 func Coupons(isin string) ([]Coupon, error) {
+	//Получить количество купонов и сравнить с размером предоставляемой порции
+	//и в цикле по порциям заполнить таблицу купонов
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*10))
 	defer cancel()
-	url := fmt.Sprintf("https://iss.moex.com/iss/statistics/engines/stock/markets/bonds/bondization/%s.json?iss.only=coupons", isin)
+	url := fmt.Sprintf("https://iss.moex.com/iss/statistics/engines/stock/markets/bonds/bondization/%s.json?iss.only=coupons.cursor&iss.meta=off", isin)
 
 	// Выполняем GET-запрос
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, http.NoBody)
@@ -264,29 +273,73 @@ func Coupons(isin string) ([]Coupon, error) {
 		return nil, err
 	}
 
+	type CouponsCursor struct {
+		Cursor struct {
+			Data [][]int64 `json:"data"`
+		} `json:"coupons.cursor"`
+	}
 	// Парсим JSON
-	var bondPayments BondPayments
-	err = json.Unmarshal(body, &bondPayments)
+	var couponsCursor CouponsCursor
+	err = json.Unmarshal(body, &couponsCursor)
 	if err != nil {
 		fmt.Println("Ошибка парсинга JSON:", err)
 		return nil, err
 	}
 
-	// Преобразуем данные в структуру Coupon
+	i := couponsCursor.Cursor.Data[0][0]
+	total := couponsCursor.Cursor.Data[0][1]
+	pagesize := couponsCursor.Cursor.Data[0][2]
+	// 0 36 20
+
 	var coupons []Coupon
-	for _, row := range bondPayments.Coupons.Data {
-		coupon := Coupon{
-			Isin:             row[0].(string),
-			Coupondate:       row[3].(string),
-			Recorddate:       row[4].(string),
-			Initialfacevalue: row[6].(float64),
-			Facevalue:        row[7].(float64),
-			Faceunit:         row[8].(string),
-			Value:            row[9].(float64),
-			Valueprc:         row[10].(float64),
-			ValueRub:         row[11].(float64),
+	for ; i < total; i = i + pagesize {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*10))
+		defer cancel()
+		url := fmt.Sprintf("https://iss.moex.com/iss/statistics/engines/stock/markets/bonds/bondization/%s.json?iss.only=coupons&iss.meta=off&start=%d", isin, i)
+
+		// Выполняем GET-запрос
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, http.NoBody)
+		if err != nil {
+			return nil, err
 		}
-		coupons = append(coupons, coupon)
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			servicelog.ErrorLog().Print(err.Error())
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		// Читаем тело ответа
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Ошибка при чтении ответа:", err)
+			return nil, err
+		}
+
+		// Парсим JSON
+		var bondPayments BondPayments
+		err = json.Unmarshal(body, &bondPayments)
+		if err != nil {
+			fmt.Println("Ошибка парсинга JSON:", err)
+			return nil, err
+		}
+
+		// Преобразуем данные в структуру Coupon
+		for _, row := range bondPayments.Coupons.Data {
+			coupon := Coupon{
+				Isin:             row[0].(string),
+				Coupondate:       row[3].(string),
+				Recorddate:       row[4].(string),
+				Initialfacevalue: row[6].(float64),
+				Facevalue:        row[7].(float64),
+				Faceunit:         row[8].(string),
+				Value:            row[9].(float64),
+				Valueprc:         row[10].(float64),
+				ValueRub:         row[11].(float64),
+			}
+			coupons = append(coupons, coupon)
+		}
 	}
 
 	return coupons, nil
@@ -345,22 +398,99 @@ func Amortizations(isin string) ([]Amortization, error) {
 }
 
 func BondIndicators(isin string) (bondIndicators, error) {
-	bI := bondIndicators{}
+	bI := bondIndicators{Isin: isin}
+
+	bond, err := moexBond(isin)
+	if err != nil {
+		return bI, err
+	}
+
+	marketData, err := moexBondMarketData(isin)
+	if err != nil {
+		return bI, err
+	}
+
+	eventDateStr := bond.MatDate
+	if bond.OfferDate != "" {
+		eventDateStr = bond.OfferDate
+	}
+	eventDate, err := time.Parse(time.DateOnly, eventDateStr)
+	if err != nil {
+		return bI, err
+	}
+
+	now := time.Now()
+	today := now.Truncate(time.Hour * 24)
+
+	var settleDate time.Time
+	if bond.SettleDate == "" {
+		settleDate = today.AddDate(0, 0, 1)
+	} else {
+		settleDate, err = time.Parse(time.DateOnly, bond.SettleDate)
+		if err != nil {
+			return bI, err
+		}
+	}
+
+	bI.FaceValue = bond.FaceValue
+	bI.AccruedInt = bond.AccruedInt
+	bI.Coupon = bond.CouponValue
+	bI.PercentPrice = marketData.Last
+	bI.Price = roundFloat(bond.FaceValue*marketData.Last/100, 2) + bond.AccruedInt
+	bI.DaysToEvent = int64(eventDate.Sub(today).Hours() / 24)
+	bI.MatDate = bond.MatDate
+	bI.OfferDate = bond.OfferDate
+
+	if marketData.Last != 0 {
+		bI.CurrentYield = roundFloat(bond.CouponPercent/marketData.Last, precision)
+		bI.NetCurrentYield = roundFloat(bond.CouponPercent*bond.FaceValue*(1-taxRate)/bI.Price/100, precision)
+	}
+
+	couponsAmount := 0.0
+	coupons, err := Coupons(isin)
+	if err != nil {
+		return bI, err
+	}
+	for _, c := range coupons {
+		paymentDate, err := time.Parse(time.DateOnly, c.Coupondate)
+		if err != nil {
+			return bI, err
+		}
+		if paymentDate.After(settleDate) {
+			couponsAmount += c.Value
+		}
+	}
+	bI.SimpleYield = roundFloat((couponsAmount+bond.FaceValue-bI.Price)/bI.Price*365/float64(bI.DaysToEvent), precision)
+
+	creditDate := settleDate.AddDate(3, 0, 0) // дата для ЛДВ
+	matTax := 0.0
+	if !eventDate.After(creditDate) && bI.Price < bond.FaceValue {
+		matTax = roundFloat((bond.FaceValue-bI.Price)*taxRate, 2)
+	}
+	bI.NetSimpleYield = roundFloat((couponsAmount*(1-taxRate)+bond.FaceValue-matTax-bI.Price)/bI.Price*365/float64(bI.DaysToEvent), precision)
+	bI.MaturityTax = matTax
+
+	return bI, nil
+}
+
+func moexBond(isin string) (Bond, error) {
+	b := Bond{Isin: isin}
+
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*10))
 	defer cancel()
-
-	url := fmt.Sprintf("https://iss.moex.com/iss/statistics/engines/stock/markets/bonds/bondization/%s.json?iss.only=amortizations", isin)
+	secProperties := "ISIN,SHORTNAME,ACCRUEDINT,FACEVALUE,MATDATE,COUPONPERIOD,COUPONPERCENT,SECNAME,FACEUNIT,COUPONPERCENT,OFFERDATE,SETTLEDATE,COUPONVALUE"
+	url := fmt.Sprintf("https://iss.moex.com/iss/engines/stock/markets/bonds/securities/%s.json?iss.meta=off&iss.only=securities,&securities.columns=%s", isin, secProperties)
 
 	// Выполняем GET-запрос
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, http.NoBody)
 	if err != nil {
-		return bI, err
+		return b, err
 	}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		servicelog.ErrorLog().Print(err.Error())
-		return bI, err
+		return b, err
 	}
 	defer resp.Body.Close()
 
@@ -368,10 +498,179 @@ func BondIndicators(isin string) (bondIndicators, error) {
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("Ошибка при чтении ответа:", err)
-		return bI, err
+		return b, err
 	}
 
 	// Парсим JSON
+	type moexBond struct {
+		Securities struct {
+			Data [][]interface{}
+		} `json:"securities"`
+	}
 
-	return bI, nil
+	var mBond moexBond
+	err = json.Unmarshal(body, &mBond)
+	if err != nil {
+		return b, err
+	}
+
+	for _, row := range mBond.Securities.Data {
+		b.ShortName, _ = row[1].(string)
+		b.AccruedInt, _ = row[2].(float64)
+		b.FaceValue, _ = row[3].(float64)
+		b.MatDate, _ = row[4].(string)
+		b.CouponPeriod = int32(row[5].(float64))
+		b.CouponPercent, _ = row[6].(float64)
+		b.SecName, _ = row[7].(string)
+		b.FaceUnit, _ = row[8].(string)
+		b.CouponPercent, _ = row[9].(float64)
+		b.OfferDate, _ = row[10].(string)
+		b.SettleDate, _ = row[11].(string)
+		b.CouponValue, _ = row[12].(float64)
+	}
+	return b, nil
 }
+
+func moexBondMarketData(isin string) (BondMarketData, error) {
+	var marketData BondMarketData
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*10))
+	defer cancel()
+	secProperties := "LAST"
+	url := fmt.Sprintf("https://iss.moex.com/iss/engines/stock/markets/bonds/securities/%s.json?iss.meta=off&iss.only=marketdata&marketdata.columns=%s", isin, secProperties)
+
+	// Выполняем GET-запрос
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, http.NoBody)
+	if err != nil {
+		return marketData, err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		servicelog.ErrorLog().Print(err.Error())
+		return marketData, err
+	}
+	defer resp.Body.Close()
+
+	// Читаем тело ответа
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Ошибка при чтении ответа:", err)
+		return marketData, err
+	}
+
+	type moexMarketData struct {
+		MarketData struct {
+			Data [][]interface{}
+		} `json:"marketdata"`
+	}
+
+	var moexData moexMarketData
+	err = json.Unmarshal(body, &moexData)
+	if err != nil {
+		return marketData, err
+	}
+
+	for _, row := range moexData.MarketData.Data {
+		marketData.Last = row[0].(float64)
+	}
+	return marketData, nil
+}
+
+func roundFloat(val float64, precision uint) float64 {
+	ratio := math.Pow(10, float64(precision))
+	return math.Round(val*ratio) / ratio
+}
+
+// func (bI *bondIndicators) fillMoex() (error) {
+// 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*10))
+// 	defer cancel()
+
+// 	url := fmt.Sprintf("https://iss.moex.com/iss/engines/stock/markets/bonds/securities/%s.json?iss.meta=off&iss.only=securities,marketdata&marketdata.columns=LAST,VALUE&securities.columns=FACEVALUE,ACCRUEDINT,MATDATE,OFFERDATE", isin)
+
+// 	// Выполняем GET-запрос
+// 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, http.NoBody)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	resp, err := http.DefaultClient.Do(req)
+// 	if err != nil {
+// 		servicelog.ErrorLog().Print(err.Error())
+// 		return err
+// 	}
+// 	defer resp.Body.Close()
+
+// 	// Читаем тело ответа
+// 	body, err := io.ReadAll(resp.Body)
+// 	if err != nil {
+// 		fmt.Println("Ошибка при чтении ответа:", err)
+// 		return err
+// 	}
+
+// 	// Парсим JSON
+// 	type moexBond struct {
+// 		Securities struct {
+// 			Data [][]interface{}
+// 		} `json:"securities"`
+// 		MarketData struct {
+// 			Data [][]float64 `json:"data"`
+// 		} `json:"marketdata"`
+// 	}
+
+// 	var prices moexBond
+// 	err = json.Unmarshal(body, &prices)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	if len(prices.MarketData.Data) == 0 {
+// 		return nil
+// 	}
+
+// 	return nil
+// }
+
+// func lastPrice(isin string) (float64, error) {
+
+// 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*10))
+// 	defer cancel()
+
+// 	url := fmt.Sprintf("https://iss.moex.com/iss/engines/stock/markets/bonds/securities/%s.json?iss.meta=off&iss.only=securities,marketdata&marketdata.columns=LAST,VALUE&securities.columns=FACEVALUE,ACCRUEDINT,MATDATE,OFFERDATE", isin)
+
+// 	// Выполняем GET-запрос
+// 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, http.NoBody)
+// 	if err != nil {
+// 		return 0, err
+// 	}
+
+// 	resp, err := http.DefaultClient.Do(req)
+// 	if err != nil {
+// 		servicelog.ErrorLog().Print(err.Error())
+// 		return 0, err
+// 	}
+// 	defer resp.Body.Close()
+
+// 	// Читаем тело ответа
+// 	body, err := io.ReadAll(resp.Body)
+// 	if err != nil {
+// 		fmt.Println("Ошибка при чтении ответа:", err)
+// 		return 0, err
+// 	}
+
+// 	// Парсим JSON
+// 	type Prices struct {
+// 		MarketData struct {
+// 			Data [][]float64 `json:"data"`
+// 		} `json:"marketdata"`
+// 	}
+
+// 	var prices Prices
+// 	err = json.Unmarshal(body, &prices)
+// 	if err != nil {
+// 		return 0, err
+// 	}
+// 	if len(prices.MarketData.Data) == 0 {
+// 		return 0, nil
+// 	}
+
+// 	return prices.MarketData.Data[0][0], nil
+// }
